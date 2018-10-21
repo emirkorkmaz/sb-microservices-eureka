fb6b11180f9c65682970b5d0416ea83207d2e14c
Emir Korkmaz <emir.korkmaz@payrov.com.tr>
2018-10-21 21:53:10 +0300

    initial

---
+++ src/main/java/com/study/sb/microservices/eureka/sbmicroserviceseureka/SbMicroservicesEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.study.sb.microservices.eureka.sbmicroserviceseureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbMicroservicesEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbMicroservicesEurekaApplication.class, args);
+	}
+}
