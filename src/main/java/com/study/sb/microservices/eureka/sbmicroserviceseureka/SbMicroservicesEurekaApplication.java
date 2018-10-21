e547a3b8831335fde0639915388fb9db9ed656fb
Emir Korkmaz <emir.korkmaz@payrov.com.tr>
2018-10-21 23:25:11 +0300

    eureka is enabled for service discovery

--- src/main/java/com/study/sb/microservices/eureka/sbmicroserviceseureka/SbMicroservicesEurekaApplication.java
+++ src/main/java/com/study/sb/microservices/eureka/sbmicroserviceseureka/SbMicroservicesEurekaApplication.java
@@ -2,8 +2,10 @@ package com.study.sb.microservices.eureka.sbmicroserviceseureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SbMicroservicesEurekaApplication {
 
 	public static void main(String[] args) {
